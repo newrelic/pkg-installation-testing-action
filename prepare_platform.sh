27313c8488ca44f31c6057fdd2295b0a94e53133
rahul <rannareddy@newrelic.com>
2025-08-26 13:21:17 +0530

    added few java platforms

--- prepare_platform.sh
+++ prepare_platform.sh
@@ -24,7 +24,8 @@ This is a bash script to make generate a Molecule configutaion.
     exit
 fi
 
-available_platforms=("al2" "al2023" "centos7" "centos8" "debian-bullseye" "debian-buster" "debian-bookworm" "redhat8" "redhat9" "redhat10" "suse15.2" "suse15.3" "suse15.4" "suse15.5" "suse15.6" "ubuntu1604" "ubuntu1804" "ubuntu2004" "ubuntu2204" "ubuntu2404")
+# available_platforms=("al2" "al2023" "centos7" "centos8" "debian-bullseye" "debian-buster" "debian-bookworm" "redhat8" "redhat9" "redhat10" "suse15.2" "suse15.3" "suse15.4" "suse15.5" "suse15.6" "ubuntu1604" "ubuntu1804" "ubuntu2004" "ubuntu2204" "ubuntu2404")
+available_platforms=("al2" "al2023" "centos7" "centos8" "debian-bullseye" "debian-buster" "debian-bookworm" "redhat8" "redhat9" "redhat10" "suse15.2" "suse15.3" "suse15.4" "suse15.5" "suse15.6" "ubuntu1604" "ubuntu1804" "ubuntu2004" "ubuntu2204" "ubuntu2404" "al2023java" "redhat8java" "suse15.6java" "ubuntu2204java" "ubuntu2404java")
 
 # check_platforms verifies that the provided platforms are available
 check_platforms() {
